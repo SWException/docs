d95e262c6b2659ce8e6fc13a2f2e22ac0cfae11d
Michele Veronesi <micheleveronesi0@gmail.com>
2020-12-12 10:37:26 +0100

    init struttura processi supporto

--- clean.sh
+++ clean.sh
@@ -7,3 +7,5 @@ find -type f -iname "*.fls" -exec rm -f {} \;
 find -type f -iname "*.log" -exec rm -f {} \;
 find -type f -iname "*.pdf" -exec rm -f {} \;
 find -type f -iname "*.gz" -exec rm -f {} \;
+find -type f -iname "*.toc" -exec rm -f {} \;
+find -type f -iname "*.out" -exec rm -f {} \;
