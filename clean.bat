6c8dfc1999631b5dee538c60e280eb54c2463543
Michele Veronesi <micheleveronesi0@gmail.com>
2020-12-12 09:37:30 +0100

    aggiunti script pulizia

---
+++ clean.bat
@@ -0,0 +1,14 @@
+:: script to clean workspace after latex build
+:: for windows environment
+
+del *.aux /s
+del .cache /s
+del .DS_Store /s
+del *.pdf /s
+del *.log /s
+del *.out /s
+del *.gz /s
+del *.toc /s
+del *.dvi /s
+del *.fls /s
+del *latexmk /s
